c753583afc4505faec294d3f558055f37ccd93ee
unknown <coder.rakibul@gmail.com>
2022-05-16 20:39:20 +0600

    server updated

--- index.js
+++ index.js
@@ -3,26 +3,27 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wnxnw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.twtll.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
-        return res.status(401).send({ message: 'Unauthorized Access' });
+        return res.status(401).send({ message: 'UnAuthorized access' });
     }
     const token = authHeader.split(' ')[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
         if (err) {
-            return res.status(403).send({ message: 'Fobidden Access' })
+            return res.status(403).send({ message: 'Forbidden access' })
         }
         req.decoded = decoded;
         next();
@@ -49,12 +50,12 @@ async function run() {
             res.send(users);
         });
 
-        app.get('admin/:email', async(req, res) => {
+        app.get('/admin/:email', async (req, res) => {
             const email = req.params.email;
-            user = await userCollection.findOne({email: email});
+            const user = await userCollection.findOne({ email: email });
             const isAdmin = user.role === 'admin';
-            res.send({admin: isAdmin});
-        });
+            res.send({ admin: isAdmin })
+        })
 
         app.put('/user/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
@@ -66,12 +67,13 @@ async function run() {
                     $set: { role: 'admin' },
                 };
                 const result = await userCollection.updateOne(filter, updateDoc);
-                res.send({ result });
+                res.send(result);
             }
-            else{
-                res.status(403).send({message: 'Forbidden'});
+            else {
+                res.status(403).send({ message: 'forbidden' });
             }
-        });
+
+        })
 
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
@@ -82,7 +84,7 @@ async function run() {
                 $set: user,
             };
             const result = await userCollection.updateOne(filter, updateDoc, options);
-            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30d' });
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
             res.send({ result, token });
         })
 
@@ -112,7 +114,17 @@ async function run() {
 
 
             res.send(services);
-        });
+        })
+
+        /**
+         * API Naming Convention
+         * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
+         * app.get('/booking/:id') // get a specific booking 
+         * app.post('/booking') // add a new booking
+         * app.patch('/booking/:id) //
+         * app.put('/booking/:id') // upsert ==> update (if exists) or insert (if doesn't exist)
+         * app.delete('/booking/:id) //
+        */
 
         app.get('/booking', verifyJWT, async (req, res) => {
             const patient = req.query.patient;
@@ -123,9 +135,8 @@ async function run() {
                 return res.send(bookings);
             }
             else {
-                return res.status(403).send({ message: 'Forbidden Access' });
+                return res.status(403).send({ message: 'forbidden access' });
             }
-
         })
 
         app.post('/booking', async (req, res) => {
@@ -139,20 +150,19 @@ async function run() {
             return res.send({ success: true, result });
         })
 
-
-
     }
-
     finally {
 
     }
 }
+
 run().catch(console.dir);
 
+
 app.get('/', (req, res) => {
-    res.send('Hello From Doctors Portal!')
+    res.send('Hello From Doctor Uncle!')
 })
 
 app.listen(port, () => {
-    console.log(`Doctors app listening on port ${port}`)
+    console.log(`Doctors App listening on port ${port}`)
 })
